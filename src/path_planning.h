830192efe43c6c007ddc639271dc3ac7a382c12e
ardop <humanoid.ardop@gmail.com>
2017-04-24 14:25:35 +0530

    Piecewise Joint Path

--- src/path_planning.h
+++ src/path_planning.h
@@ -30,8 +30,8 @@ using namespace arma;
 using namespace std;
 
 // Defining standar mats
-mat mat_zero;
-mat_zero << 0;
+mat zero_mat;
+zero_mat << 0;
 
 void print_check_matrix(const mat& A, const mat& B)
 {
@@ -79,7 +79,7 @@ mat joint_path_linear(const mat& theta_a, const mat& theta_b, int n0, int nf)
 		
 		mat theta_c;
 		theta_c = a*x;
-		theta_c = join_horiz(theta_c, mat_zero);
+		theta_c = join_horiz(theta_c, zero_mat);
 		
 		// Appending to the matrix being returned
 		configuration_history = join_vert(configuration_history; theta_c);
@@ -94,6 +94,8 @@ mat joint_path_linear(const mat& theta_a, const mat& theta_b, int n0, int nf)
 
 mat joint_path_cubic(const mat& theta_a, const mat& theta_b, int n0, int nf, double dq0, double dqf)
 {
+	mat configuration_history;
+
 	// Time matrix
 	mat A;
 	A << pow(n0, 3) << pow(n0, 2) << n0 << 1 << endr << 3*pow(n0, 2) << 2*n0 << 1 << 0 << endr << pow(nf, 3) << pow(nf, 2) << nf << 1 << endr << 3*pow(nf, 2) << 2*nf << 1 << 0;
@@ -123,7 +125,7 @@ mat joint_path_cubic(const mat& theta_a, const mat& theta_b, int n0, int nf, dou
 		
 		mat theta_c;
 		theta_c = a*x;
-		theta_c = join_horiz(theta_c, mat_zero);
+		theta_c = join_horiz(theta_c, zero_mat);
 		
 		// Appending to the matrix being returned
 		configuration_history = join_vert(configuration_history; theta_c);
@@ -132,6 +134,127 @@ mat joint_path_cubic(const mat& theta_a, const mat& theta_b, int n0, int nf, dou
 	return configuration_history;
 
 }
+
+mat joint_path_piecewise(mat const& theta_a, mat const& theta_b, int n0, int n1, int n2, int nf)
+{
+	mat configuration_history;
+
+	// Getting configurations in between the motion to stabilize the solutions
+	// A cubic equation is used for this purpose
+
+	mat A0;
+	A0 << pow(n0, 3) << pow(n0, 2) << n0 << 1 << endr << 3*pow(n0, 2) << 2*n0 << 1 << 0 << endr << pow(nf, 3) << pow(nf, 2) << nf << 1 << endr << 3*pow(nf, 2) << 2*nf << 1 << 0;
+
+	mat dq0_mat, dqf_mat;
+
+	// Setting initial velocity and final velocity to zero
+	dq0_mat << 0 << 0 << 0 << 0;
+	dqf_mat << 0 << 0 << 0 << 0;
+
+	mat B0;
+	B0 = join_vert(join_vert(theta_a.cols(1, 4), dq0_mat), join_vert(theta_b.cols(1, 4), dqf_mat));
+
+	mat x0;
+	x0 = solve(A0, B0);
+
+	// printing to check solution
+	print_check_matrix(A0, B0);
+
+	// Calculating configurations at n1 and n2
+	mat tmp_q1, tmp_q2, tmp_a1, tmp_a2;
+
+	tmp_a1 << pow(n1, 3) << pow(n1, 2) << n1 << 1;
+	tmp_a2 << pow(n2, 3) << pow(n2, 2) << n2 << 1;
+
+	tmp_q1 = tmp_a1*x0;
+	tmp_q2 = tmp_a2*x0;
+
+	// Calculating the velocity that makes the constant velocity phase and transition from and to rest possible
+	mat dqc;
+	dqc = (tmp_q1 - tmp_q2)/(n1-n2);
+
+	// Cubic equation from rest to constant velocity -------------------------------------------------------------------------------
+	mat A1;
+	A1 << pow(n0, 3) << pow(n0, 2) << n0 << 1 << endr << 3*pow(n0, 2) << 2*n0 << 1 << 0 << endr << pow(n1, 3) << pow(n1, 2) << n1 << 1 << endr << 3*pow(n1, 2) << 2*n1 << 1 << 0;
+
+	mat B1;
+	B1 = join_vert(join_vert(theta_a.cols(1, 4), dq0_mat), join_vert(tmp_q1, dqc));
+
+	mat x1;
+	x1 = solve(A1, B1);
+
+	// printing to check solution
+	print_check_matrix(A1, B1);
+
+	// Calculating constant of integrtion for the constant velocity phase (linear equation)
+	mat c1;
+	c1 = tmp_a1*x1;
+	c1 = c1 - (dqc*n1);
+
+	// Function parameters for the second phase ------------------------------------------------------------------------------------
+	mat x2;
+	x2 = join_vert(dqc, c1);
+
+	// Cubic function for motion to rest phase -------------------------------------------------------------------------------------
+
+	mat A3;
+	A3 << pow(nf, 3) << pow(nf, 2) << nf << 1 << endr << 3*pow(nf, 2) << 2*nf << 1 << 0 << endr << pow(n2, 3) << pow(n2, 2) << n2 << 1 << endr << 3*pow(n2, 2) << 2*n2 << 1 << 0;
+
+	mat B3;
+	B3 = join_vert(join_vert(theta_b.cols(1, 4), dqf_mat), join_vert(tmp_q2, dqc));
+
+	mat x3;
+	x3 = solve(A3, B3);
+
+	// printing to check solution
+	print_check_matrix(A3, B3);
+
+
+	// Trajectory is calculated in 3 phases. The trjectory history is updated in all three phases to form the combined path
+
+	for(int i=n0;i<=n1;i++)
+	{
+		mat a;
+		a << pow(i, 3) << pow(i, 2) << i << 1;
+
+		mat theta_c;
+		theta_c = a*x1;
+		theta_c = join_horiz(theta_c, zero_mat);
+		
+		// Appending to the matrix being returned
+		configuration_history = join_vert(configuration_history; theta_c);
+	}
+
+	for(int i=n1;i<=n2;i++)
+	{
+		mat a;
+		a << i << 1;
+
+		mat theta_c;
+		theta_c = a*x2;
+		theta_c = join_horiz(theta_c, zero_mat);
+		
+		// Appending to the matrix being returned
+		configuration_history = join_vert(configuration_history; theta_c);
+	}
+
+	for(int i=n2;i<=nf;i++)
+	{
+		mat a;
+		a << pow(i, 3) << pow(i, 2) << i << 1;
+
+		mat theta_c;
+		theta_c = a*x3;
+		theta_c = join_horiz(theta_c, zero_mat);
+		
+		// Appending to the matrix being returned
+		configuration_history = join_vert(configuration_history; theta_c);
+	}
+
+	return configuration_history;
+}
+
+
 
 
 
